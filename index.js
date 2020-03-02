fad999364a800b4d01f773845d567e3cb49a49b3
unknown <jamie-crowther@live.nmit.ac.nz>
2020-03-02 18:05:31 +1300

    Added index.js and played around with js

---
+++ index.js
@@ -0,0 +1,68 @@
+console.log ("Hello Class");
+
+var firstName;
+
+firstName = "Jamie";
+
+console.log(firstName);
+
+var lastName = "Crowther";
+
+console.log(lastName);
+
+console.log(firstName,lastName);
+
+var info;
+
+info="Whie you CAN use a comma to add a space, this way doesn't really work with arrays";
+
+console.log(info);
+
+console.log(firstName + " " + lastName);
+
+console.log("To show the work that you've done in the terminal, type Node then add a space then filename e.g index.js");
+
+var number1;
+
+number1=10;
+
+var number2;
+
+number2=5;
+
+console.log(number1+number2);
+
+console.log("You can show the sum of an equation by putting a + - / * inbetween the variables")
+
+console.log("+ for addition, - for minus, / for division, * for multiplication")
+
+var result;
+
+result=number1-number2;
+
+console.log(result);
+
+console.log("You can also make a new variable that will show the sum by setting var=number1+number2");
+
+var question1;
+
+question1=("What is 10-5");
+
+console.log(question1);
+
+console.log(result);
+
+console.log("What is 10x5");
+
+console.log(number1*number2);
+
+console.log("What is 10 divided by 5");
+
+console.log(number1/number2);
+
+console.log("What is 10+5");
+
+console.log(number1+number2);
+
+console.log(number1**number2);
+
