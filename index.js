207fb49f4f9d0299bfbcaf2222a53b0bea9a2798
unknown <jamie-crowther@live.nmit.ac.nz>
2020-03-02 19:15:24 +1300

    More playing around with index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ console.log("+ for addition, - for minus, / for division, * for multiplication")
 
 var result;
 
-result=number1-number2;
+result=number1+number2;
 
 console.log(result);
 
@@ -64,5 +64,35 @@ console.log("What is 10+5");
 
 console.log(number1+number2);
 
+console.log("You can figure out exponents too, eg 5**2 with show you 5 squared, 10**5 will do 10x10x10x10x10");
+
+console.log("What is 10 to the power of 5");
+
 console.log(number1**number2);
+
+console.log("You can write the code to display the question like this");
+
+console.log("it wont add them together because the + is in a text bracket")
+
+console.log(number1, "+", number2);
+
+console.log(number1 + number2);
+
+console.log("Below I will try to do the whole equation in one line");
+
+console.log(number1, "+", number2, "=", result);
+
+console.log(number1, "+", number2, "=", number1+number2);
+
+console.log("Now without the commas");
+
+console.log(number1 + " + " + number2 + " = " + result);
+
+console.log("That one was a lot harder to think of, i encountered a lot of errors when trying to show in terminal");
+
+console.log("But through much trial and error, I managed to get a code that works ^");
+
+console.log("Using the commas seems a lot nicer because of the yellow numbers")
+
+console.log("So is there a way to make the number show up yellow without using comma ?");
 
